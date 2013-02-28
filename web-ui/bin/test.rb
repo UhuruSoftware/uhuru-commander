b151b86fc97f17c8593345a515b081366f7e8b84
mihai.buzgau <mihai.buzgau@uhurusoftware.com>
2013-02-28 02:40:52 -0800

    test for generationg network ips
    Change-Id: Ie97f3ca6108688cfbfcc95ad2bbddb58f0ae3387

--- web-ui/bin/test.rb
+++ web-ui/bin/test.rb
@@ -1,43 +1,96 @@
 require 'ip_admin'
 
-#def ip_range(range, &block)
-#  start, delta = range.split("-").map do |ipstr|
-#    ipstr.split(".").map do |number| number.to_i end
-#  end
-#
-#  (0..3).each do |pos|
-#    delta[pos] -= start[pos]
-#  end
-#
-#  (0..delta[0]).each do |d0|
-#    (0..delta[1]).each do |d1|
-#      (0..delta[2]).each do |d2|
-#        (0..delta[3]).each do |d3|
-#          yield [start[0] + d0, start[1] + d1, start[2] + d2, start[3] +
-#              d3]
-#        end
-#      end
-#    end
-#  end
-#end
 
-#ip_range("192.167.1.1-192.169.2.5") do |ip|
-#  pp ip
-#end
+user_ip_range_start = "10.0.1.100"
+user_ip_range_end = "10.0.2.190"
+user_ip_subnet_mask = "255.255.0.0"
+user_ip_gateway = "10.0.3.162"
+
+reserved_ip = ""
+range = ""
+
+
+user_ip_subnet_mask_bits = IPAdmin.unpack_ip_netmask(IPAdmin.pack_ip_netmask(user_ip_subnet_mask))
 
 list = IPAdmin.range :Boundaries => [
-    IPAdmin::CIDR.new(:CIDR => "192.167.255.1"),
-    IPAdmin::CIDR.new(:CIDR => "192.168.1.5"),
+    IPAdmin::CIDR.new(:CIDR => user_ip_range_start),
+    IPAdmin::CIDR.new(:CIDR => user_ip_range_end),
     ]
 
-p list.count
+
+withnetmask = IPAdmin::CIDR.new("#{user_ip_range_start}/#{user_ip_subnet_mask_bits}")
+
+subnet_list = withnetmask.enumerate
 
 
-cidr4 = IPAdmin::CIDR.new('192.168.1.1/24')
-list = cidr4.enumerate
 
-p list.count
+start_range = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => subnet_list.first), IPAdmin::CIDR.new(:CIDR => user_ip_range_start)]
+end_range = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => user_ip_range_end), IPAdmin::CIDR.new(:CIDR => subnet_list.last)]
 
-#[byte].[byte].[byte].[byte]/[byte]
-#[byte].[byte].[byte].[byte][whitespace] - [byte].[byte].[byte].[byte]
+reserved_ips = []
+
+if start_range.include? user_ip_gateway
+  gw_range_start = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => start_range.first), IPAdmin::CIDR.new(:CIDR => user_ip_gateway)], :Inclusive => true
+  gw_range_end = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => user_ip_gateway), IPAdmin::CIDR.new(:CIDR => start_range.last)], :Inclusive => true
+
+  gw_range_start.delete(user_ip_gateway)
+  gw_range_end.delete(user_ip_gateway)
+
+  reserved_ips << [gw_range_start.first, gw_range_start.last]
+  reserved_ips << [gw_range_end.first, gw_range_end.last]
+else
+  reserved_ips << [start_range.first, start_range.last]
+end
+
+if end_range.include? user_ip_gateway
+  gw_range_start = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => end_range.first), IPAdmin::CIDR.new(:CIDR => user_ip_gateway)], :Inclusive => true
+  gw_range_end = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => user_ip_gateway), IPAdmin::CIDR.new(:CIDR => end_range.last)], :Inclusive => true
+
+  gw_range_start.delete(user_ip_gateway)
+  gw_range_end.delete(user_ip_gateway)
+
+
+  reserved_ips << [gw_range_start.first, gw_range_start.last]
+  reserved_ips << [gw_range_end.first, gw_range_end.last]
+else
+  reserved_ips << [end_range.first, end_range.last]
+end
+
+
+puts "Pelerinul shi-a facut magia."
+
+p "#{subnet_list.first}/#{user_ip_subnet_mask_bits}"
+
+reserved_ips.each do |ips|
+  p ips
+end
+
+
+##############################################################################################
+
+
+all_ips = []
+
+reserved_ips.each do |ips|
+  range = IPAdmin.range :Boundaries => [IPAdmin::CIDR.new(:CIDR => ips[0]), IPAdmin::CIDR.new(:CIDR => ips[1])], :Inclusive => true
+
+  all_ips += range
+end
+
+all_ips << user_ip_gateway
+
+withnetmask = IPAdmin::CIDR.new("10.0.0.0/16")
+
+users_list = withnetmask.enumerate - all_ips
+users_list.delete(withnetmask.first)
+users_list.delete(withnetmask.last)
+
+
+puts "Pelerinul reloaded"
+
+users_list = IPAdmin.sort users_list
+
+p "#{withnetmask.netmask_ext()}"
+p "#{users_list.first}-#{users_list.last}"
+
 
