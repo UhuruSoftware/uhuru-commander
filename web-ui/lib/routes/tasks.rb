9f843634af72ea6b804e104faddd592b77603139
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2013-04-12 15:58:46 +0300

    Bugfixes for UH-1544, UH-1553, UH-1555, UH-1557, UH-1560, UH-1561, UH-1567
    Change-Id: If2814d011210cc9cafce0e0eba0c5d23eec7e601

--- web-ui/lib/routes/tasks.rb
+++ web-ui/lib/routes/tasks.rb
@@ -30,5 +30,29 @@ module Uhuru::BoshCommander
       action_on_done = "Click <a href='javascript:history.back()'>here</a> to go back."
       redirect Logs.log_url(request_id, action_on_done)
     end
+
+    get '/loop' do
+      request_id = CommanderBoshRunner.execute_background(session) do
+        start = Time.now
+        while (Time.now - start) < 300
+          color = Random.rand(4)
+
+          if color == 0
+            say "#{300 - (Time.now - start)} seconds remaining..."
+          elsif color == 1
+            say "#{300 - (Time.now - start)} seconds remaining...".red
+          elsif color == 2
+            say "#{300 - (Time.now - start)} seconds remaining...".green
+          elsif color == 3
+            say "#{300 - (Time.now - start)} seconds remaining...".yellow
+          end
+
+          sleep 2
+        end
+      end
+
+      action_on_done = "Click <a href='/'>here</a> to go home."
+      redirect Logs.log_url(request_id, action_on_done)
+    end
   end
 end
