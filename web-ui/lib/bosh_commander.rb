535594e161bfb0800ef6a2678c042599cbd046c2
marius.ivan <marius.ivan@uhurusoftware.com>
2013-06-06 06:43:44 -0700

    new route for version page added
    Change-Id: I6c57f325b9c736db71058065fcfda575c6b5dfb0

--- web-ui/lib/bosh_commander.rb
+++ web-ui/lib/bosh_commander.rb
@@ -41,6 +41,7 @@ require "routes/route_base"
 require "routes/ssh"
 require "routes/tasks"
 require "routes/users"
+require "routes/versions"
 require "routes/monitoring"
 require "routes/vm"
 
@@ -65,6 +66,15 @@ module Uhuru::BoshCommander
     use Monitoring
 
     get '/' do
+      r = Random.new
+      session[:new_versions] = true
+
+      #if r.rand(0...1000) % 2 == 0
+      #  session[:new_versions] = true
+      #else
+      #  session[:new_versions] = false
+      #end
+
       redirect '/infrastructure'
     end
 
