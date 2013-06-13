3575f98741d16b3756510a8b38fcedc57f4b4a17
marius.ivan <marius.ivan@uhurusoftware.com>
2013-06-13 02:52:43 -0700

    products table added
    Change-Id: Ib319ab4fccbd041fd950fe17722c0720e53fdc77

--- web-ui/lib/routes/versions.rb
+++ web-ui/lib/routes/versions.rb
@@ -1,68 +1,18 @@
 module Uhuru::BoshCommander
-  class Clouds < RouteBase
+  class Versions < RouteBase
     get '/new_versions' do
       session[:new_versions] = false
       redirect '/versions'
     end
 
     get '/versions' do
-      tables = []
 
-
-      hash1 = { :title              => 'First',
-                :body           => {
-                    :versions           => [ '3.0.0', '3.5.0', '4.0', '4.2', '4.3', '4.5' ],
-                    :descriptions       => [ 'a simple and small descriptions 1', 'dddddddddddddddddddddddddddd ddddddddddddddddddddddd ddddddddddddddddddddddddddd dddddddd ddddddddddddddd ddddddddddddddddd' ],
-                    :dependencies       => [ 'ruby 1.9.2, sinatra, rails framework', 'none', 'nodeJs<br /> meteor framework' ],
-                    :actions            => [
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'false', :delete => 'true'},
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'true', :delete => 'false'},
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'false', :delete => 'true'},
-                    ]
-                }
-      }
-
-      hash2 = { :title              => 'Second table....',
-                :body           => {
-                    :versions           => [ '3.0.0', '3.5.0', '4.0', '4.2', '4.3', '4.5' ],
-                    :descriptions       => [ 'a simple and small descriptions 1', 'tacdasdsadasdadasdsdasdasdpac' ],
-                    :dependencies       => [ 'ruby 1.9.2, sinatra, rails framework', 'none', 'nodeJs<br /> meteor framework' ],
-                    :actions            => [
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'false', :delete => 'true'},
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'true', :delete => 'false'},
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'false', :delete => 'true'},
-                    ]
-                }
-      }
-
-      hash3 = { :title              => 'And the last table',
-                :body           => {
-                    :versions           => [ '3.0.0', '3.5.0', '4.0', '4.2', '4.3', '4.5' ],
-                    :descriptions       => [ 'a simple and small descriptions 1', '' ],
-                    :dependencies       => [ 'ruby 1.9.2, sinatra, rails framework', 'none', 'nodeJs<br /> meteor framework' ],
-                    :actions            => [
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'false', :delete => 'true'},
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'true', :delete => 'false'},
-                        {:install => 'true', :download => 'true', :delete => 'true'},
-                        {:install => 'false', :download => 'false', :delete => 'true'},
-                    ]
-                }
-      }
-
-      tables.push(hash1, hash2, hash3)
+      products = Uhuru::BoshCommander::Versioning::Product.get_products
 
       render_erb do
         template :versions
         layout :layout
-        var :tables, tables
+        var :products, products
         help 'versions'
       end
     end
@@ -70,24 +20,10 @@ module Uhuru::BoshCommander
 end
 
 
-#tables.each do |table|
-#  elements = table[:body][:versions].count
-#  counter = 0
-#  keys = table[:body].keys
+#@t << {:path => '/clouds', :href => '/clouds', :name => p[1].label}
 #
-#
-#  while counter <= elements - 1 do
-#    keys.each do |key|
-#      if key.to_s == 'versions'
-#        puts table[:body][key.to_sym][counter]
-#      end
-#
-#      if key.to_s == 'actions'
-#        if table[:body][key.to_sym][counter] != nil
-#          puts table[:body][key.to_sym][counter][:install]
-#        end
-#      end
-#    end
-#    counter += 1
-#  end
+#product[1].versions.each do |current_ver|
+#  current_ver[1].version
 #end
+
+
