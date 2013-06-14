0b3093469bd08cd0a8e47b0a09e2982480567dc8
marius.ivan <marius.ivan@uhurusoftware.com>
2013-06-14 07:51:41 -0700

    script currently refresh with random values
    Change-Id: I120acc83bf1994ff2a289011ea95f39b9bd6c06d

--- web-ui/lib/routes/versions.rb
+++ web-ui/lib/routes/versions.rb
@@ -5,30 +5,39 @@ module Uhuru::BoshCommander
       redirect '/versions'
     end
 
-
     post '/versions/read_states' do
       r = Random.new
-      current_state = nil
-
+      state = 'error'
       Uhuru::BoshCommander::Versioning::Product.get_products.each do |product|
         if product[1].name == params[:product]
           product[1].versions.each do |version|
             if version[1].version == params[:version]
-              #puts "parameters: " + params[:product] + params[:version]
-              #puts "real_values :" + product[1].name + version[1].version
-
-              current_state = r.rand(0...1000)
-              puts current_state
-              return current_state.to_i.to_s
-            else
-              return "loading..."
+              Uhuru::BoshCommander::CommanderBoshRunner.execute(session) do
+                state = r.rand(0...1000) #version[1].get_state
+                puts state
+              end
             end
           end
-        else
-          return
         end
       end
-      return "loading..."
+
+      #if state == 1
+      #  state = "Remote Only"
+      #elsif state == 2
+      #  state = "Downloading"
+      #elsif state == 3
+      #  state = "Local"
+      #elsif state == 4
+      #  state = "Local Preparing"
+      #elsif state == 5
+      #  state = "Available"
+      #elsif state == 6
+      #  state = "Deployed"
+      #else state == "error"
+      #  state = "Loading ..."
+      #end
+
+      return state.to_s
     end
 
     get '/versions' do
