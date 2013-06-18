f9f55a85b9a8970c73cdd14d5981ee85a14d9880
marius.ivan <marius.ivan@uhurusoftware.com>
2013-06-18 05:33:13 -0700

    latest changes to the versions table
    Change-Id: I87644d49fbe3b27a5eb59a74d660f7e2f687a9e1

--- web-ui/lib/routes/versions.rb
+++ web-ui/lib/routes/versions.rb
@@ -6,37 +6,42 @@ module Uhuru::BoshCommander
     end
 
     post '/versions/read_states' do
-      state = 'error'
+      state = nil
 
-      Uhuru::BoshCommander::Versioning::Product.get_products.each do |product|
-        if product[1].name == params[:product]
-          product[1].versions.each do |version|
-            if version[1].version == params[:version]
-              Uhuru::BoshCommander::CommanderBoshRunner.execute(session) do
+      begin
+        Uhuru::BoshCommander::Versioning::Product.get_products.each do |product|
+          if product[1].name == params[:product]
+            product[1].versions.each do |version|
+              if version[1].version == params[:version]
+                Uhuru::BoshCommander::CommanderBoshRunner.execute(session) do
 
-                #state = r.rand(1...7)    #version[1].get_state
-                state = version[1].get_state.to_s
+                  #state = r.rand(1...7)    #version[1].get_state
+                  state = version[1].get_state.to_s
 
-                if state == '1'
-                  state = "Remote Only"
-                elsif state == '2'
-                  state = "Downloading"
-                elsif state == '3'
-                  state = "Local"
-                elsif state == '4'
-                  state = "Local Preparing"
-                elsif state == '5'
-                  state = "Available"
-                elsif state == '6'
-                  state = "Deployed"
-                else
-                  state = "Loading ..."
+                  if state == '1'
+                    state = '1'
+                  elsif state == '2'
+                    state = '2'
+                  elsif state == '3'
+                    state = '3'
+                  elsif state == '4'
+                    state = '4'
+                  elsif state == '5'
+                    state = '5'
+                  elsif state == '6'
+                    state = '6'
+                  else
+                    state = '7'
+                  end
+
                 end
-
               end
             end
           end
         end
+      rescue Exception => e
+        $logger.warn "Could not read states - #{e.message} : #{e.backtrace}"
+        return '7'
       end
 
       return state.to_s
