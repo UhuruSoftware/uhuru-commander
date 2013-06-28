6bd04247d1ec1b691469d4df2bf637615bf09813
marius.ivan <marius.ivan@uhurusoftware.com>
2013-06-28 07:58:58 -0700

    changes in the versioning and download threading
    Change-Id: I6e6f34dfe183e9cda37ed0a3c37dda412c314f68

---
+++ web-ui/lib/products_checker.rb
@@ -0,0 +1,22 @@
+module Uhuru::BoshCommander
+  class ProductsChecker
+
+    def self.start_checking
+      Thread.new do
+        1.upto(Float::INFINITY) do
+          check_for_updates
+
+          # TODO: this variable needs to be configurable
+          sleep(1)
+        end
+      end
+    end
+
+    def self.check_for_updates
+      # TODO: was not yet properly tested
+      return Uhuru::BoshCommander::Versioning::Product.download_manifests
+    end
+
+  end
+end
+
