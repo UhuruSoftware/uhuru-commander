f3e113bb44fd68db301355b1175b0224bcf4e2d9
marius.ivan <marius.ivan@uhurusoftware.com>
2013-07-03 04:06:55 -0700

    upload and download for stemcells and releases
    Change-Id: If738b89c1f162d494a7aadd7133958e1419a0489

--- web-ui/lib/routes/versions.rb
+++ web-ui/lib/routes/versions.rb
@@ -16,7 +16,6 @@ module Uhuru::BoshCommander
             if version[1].version == params[:version]
               Uhuru::BoshCommander::CommanderBoshRunner.execute(session) do
                 version[1].download_from_blobstore
-                puts 'started'
               end
             end
           end
@@ -37,6 +36,7 @@ module Uhuru::BoshCommander
             if version[1].version == params[:version]
               Uhuru::BoshCommander::CommanderBoshRunner.execute(session) do
                 progress = version[1].download_progress
+                puts progress
               end
             end
           end
@@ -46,6 +46,83 @@ module Uhuru::BoshCommander
       # needs to return a json array in order to show the progress span %
       return progress[0].to_s
     end
+
+    post '/delete_stemcell' do
+      products_dir = Uhuru::BoshCommander::Versioning::Product.version_directory
+      product_dir = File.join(products_dir, params[:name])
+      FileUtils.rm_rf("#{product_dir}/#{params[:version]}")
+
+      request_id = CommanderBoshRunner.execute_background(session) do
+        begin
+          stemcell = Uhuru::BoshCommander::Stemcell.new
+          stemcell.delete(params[:name], params[:version])
+        rescue Exception => e
+          $logger.error "#{e.message} - #{e.backtrace}"
+        end
+      end
+
+      action_on_done = "Stemcell '#{params[:name]}' - '#{params[:version]}' deleted. Click <a href='/versions'>here</a> to return to versions panel."
+      redirect Logs.log_url(request_id, action_on_done)
+
+      redirect '/versions'
+    end
+
+    post '/delete_software' do
+      products_dir = Uhuru::BoshCommander::Versioning::Product.version_directory
+      product_dir = File.join(products_dir, params[:name])
+      FileUtils.rm_rf("#{product_dir}/#{params[:version]}")
+
+      request_id = CommanderBoshRunner.execute_background(session) do
+        begin
+          release = Uhuru::BoshCommander::Release.new
+          release.delete(params[:name], params[:version])
+        rescue Exception => e
+          $logger.error "#{e.message} - #{e.backtrace}"
+        end
+      end
+
+      action_on_done = "Release '#{params[:name]}' - '#{params[:version]}' deleted. Click <a href='/versions'>here</a> to return to versions panel."
+      redirect Logs.log_url(request_id, action_on_done)
+
+      redirect '/versions'
+    end
+
+
+    post '/upload_stemcell' do
+      products_dir = Uhuru::BoshCommander::Versioning::Product.version_directory
+      product_dir = File.join(products_dir, params[:name])
+
+      request_id = CommanderBoshRunner.execute_background(session) do
+        begin
+          stemcell = Uhuru::BoshCommander::Stemcell.new
+          stemcell.upload("#{product_dir}/#{params[:version]}/#{params[:version]}.tar.gz")
+        rescue Exception => e
+          $logger.error "#{e.message} - #{e.backtrace}"
+        end
+      end
+
+      action_on_done = "Release uploaded. Click <a href='/versions'>here</a> to return to versions panel."
+      redirect Logs.log_url(request_id, action_on_done)
+    end
+
+    post '/upload_software' do
+      products_dir = Uhuru::BoshCommander::Versioning::Product.version_directory
+      product_dir = File.join(products_dir, params[:name])
+
+      request_id = CommanderBoshRunner.execute_background(session) do
+        begin
+          release = Uhuru::BoshCommander::Release.new
+          release.upload("#{product_dir}/#{params[:version]}")#'/home/mitza/code/private-cf-release/dev_releases/app-cloud-122.3-dev.yml'
+
+        rescue Exception => e
+          $logger.error "#{e.message} - #{e.backtrace}"
+        end
+      end
+
+      action_on_done = "Release uploaded. Click <a href='/versions'>here</a> to return to versions panel."
+      redirect Logs.log_url(request_id, action_on_done)
+    end
+
 
 
     get '/versions' do
