595e0515427363206a47306ffcb316b005130573
florindragos <florind@uhurusoftware.com>
2013-07-12 14:58:18 +0300

    added increased http timeout options in publisher

--- publisher/lib/client.rb
+++ publisher/lib/client.rb
@@ -10,25 +10,27 @@ module Uhuru
           end
           @bsc_provider= options["blobstore_provider"]
           @bsc_options= options["blobstore_options"]
+
+          @blobstore_client = Bosh::Blobstore::Client.create(@bsc_provider, @bsc_options)
         end
 
         def upload(id, content)
-          blobstore_client = Bosh::Blobstore::Client.create(@bsc_provider, @bsc_options)
-          blobstore_client.create(content, id)
+          if blob_exists?(id)
+            delete(id)
+          end
+          @blobstore_client.create(content, id)
         end
 
         def get(id)
-          blobstore_client = Bosh::Blobstore::Client.create(@bsc_provider, @bsc_options)
-          if blobstore_client.exists?(id)
-            return blobstore_client.get(id)
+          if @blobstore_client.exists?(id)
+            return @blobstore_client.get(id)
           else
             return nil
           end
         end
 
         def delete(id)
-          blobstore_client = Bosh::Blobstore::Client.create(@bsc_provider, @bsc_options)
-          blobstore_client.delete(id)
+          @blobstore_client.delete(id)
         end
 
         def get_products
@@ -40,8 +42,7 @@ module Uhuru
         end
 
         def blob_exists?(blob_id)
-          blobstore_client = Bosh::Blobstore::Client.create(@bsc_provider, @bsc_options)
-          blobstore_client.exists?(blob_id)
+          @blobstore_client.exists?(blob_id)
         end
       end
     end
